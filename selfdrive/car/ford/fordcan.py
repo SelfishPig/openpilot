40320a23315c819cc2ee194829344be8ed4d19a9
SelfishPig <62257049+SelfishPig@users.noreply.github.com>
2022-12-23 16:48:47 -0600

    Update fordcan.py

--- selfdrive/car/ford/fordcan.py
+++ selfdrive/car/ford/fordcan.py
@@ -21,8 +21,8 @@ def spam_cancel_button(packer):
   }
   return packer.make_can_msg("Steering_Buttons", 0, values)
 
-def ParkAid_Data(packer, active, apply_steer, sappControlState, standstill):
-  # sappState 1 = APA Off, 2 = APA On | sappControl 0 = No request, 1 = Request
+def ParkAid_Data(packer, active, apply_steer, sappControlState):
+  # apaOn 1 = APA Off, 2 = APA On | apaReq 0 = No angle request, 1 = Request
   # No angle request at standstill because it causes sporadic steering wheel drift.
   if sappControlState == 1 and active:
     apaOn = 2
