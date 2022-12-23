d84180c910c23917a7613f48f738068f92046f47
SelfishPig <62257049+SelfishPig@users.noreply.github.com>
2022-12-23 12:35:10 -0600

    Update values.py

--- selfdrive/car/ford/values.py
+++ selfdrive/car/ford/values.py
@@ -1,20 +1,13 @@
-from selfdrive.car import dbc_dict
-from collections import namedtuple
+from selfdrive.car import AngleRateLimit, dbc_dict
 from cereal import car
 Ecu = car.CarParams.Ecu
-
-AngleRateLimit = namedtuple('AngleRateLimit', ['speed_points', 'max_angle_diff_points'])
-AngleLimit = namedtuple('AngleLimit', ['speed_points', 'max_angle_points'])
 
 class CarControllerParams:
   APA_STEP = 2 # 50hz
 
   # These rate limits are also enforced by the Panda safety code.
-  RATE_LIMIT_UP = AngleRateLimit(speed_points=[0., 5., 15.], max_angle_diff_points=[7.5, 1.2, .225])
-  RATE_LIMIT_DOWN = AngleRateLimit(speed_points=[0., 5., 15.], max_angle_diff_points=[7.5, 5.25, 0.6])
-  
-  # Angle limits
-  ANGLE_LIMIT = AngleLimit(speed_points=[0., 5., 15.], max_angle_points=[180., 90., 45.])
+  ANGLE_RATE_LIMIT_UP = AngleRateLimit(speed_bp=[0., 5., 15.], angle_v=[7.5, 1.2, .225])
+  ANGLE_RATE_LIMIT_DOWN = AngleRateLimit(speed_bp=[0., 5., 15.], angle_v=[7.5, 5.25, 0.6])
 
 class CAR:
   F150 = "F150"
