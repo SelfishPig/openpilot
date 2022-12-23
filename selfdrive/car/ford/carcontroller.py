7cbbd733c5b08d82252d3f14c0f910ba2bf35ae9
SelfishPig <62257049+SelfishPig@users.noreply.github.com>
2022-12-23 12:27:19 -0600

    Update carcontroller.py

--- selfdrive/car/ford/carcontroller.py
+++ selfdrive/car/ford/carcontroller.py
@@ -2,42 +2,39 @@ import math
 from common.numpy_fast import interp, clip
 from selfdrive.car.ford.fordcan import spam_cancel_button, ParkAid_Data, EngVehicleSpThrottle2, BrakeSysFeatures
 from selfdrive.car.ford.values import CarControllerParams
+from selfdrive.car import apply_std_steer_angle_limits
 from opendbc.can.packer import CANPacker
 from selfdrive.config import Conversions as CV
 
 class CarController():
   def __init__(self, dbc_name, CP, VM):
     self.packer = CANPacker(dbc_name)
-    self.last_angle = 0
+    self.apply_angle_last = 0
 
   def update(self, c, enabled, CS, frame, actuators, cruise_cancel):
     can_sends = []
 
+    apply_angle = self.apply_angle_last
+    
     if cruise_cancel:
       can_sends.append(spam_cancel_button(self.packer))
 
-    apply_speed = CS.out.vEgoRaw * CV.MS_TO_KPH
-    apply_angle = CS.out.steeringAngleDeg
-
-    if enabled:
-      apply_speed = 0
-      if CS.sappControlState == 2:
-        apply_angle = actuators.steeringAngleDeg
-        steer_up = (self.last_angle * apply_angle > 0. and abs(apply_angle) > abs(self.last_angle))
-        rate_limit = CarControllerParams.RATE_LIMIT_UP if steer_up else CarControllerParams.RATE_LIMIT_DOWN
-        max_angle_diff = interp(CS.out.vEgo, rate_limit.speed_points, rate_limit.max_angle_diff_points)
-        apply_angle = clip(apply_angle, (self.last_angle - max_angle_diff), (self.last_angle + max_angle_diff))    
-        angle_limit = CarControllerParams.ANGLE_LIMIT
-        max_angle = interp(CS.out.vEgo, angle_limit.speed_points, angle_limit.max_angle_points)
-        apply_angle = clip(apply_angle, -max_angle, max_angle)
+    if (frame % CarControllerParams.APA_STEP) == 0:
+      if enabled:
+        apply_speed = 0
+        if c.active and CS.sappControlState == 2:
+          apply_angle = apply_std_steer_angle_limits(actuators.steeringAngleDeg, self.apply_angle_last, CS.out.vEgo, CarControllerParams)
+        else:
+          apply_angle = CS.out.steeringAngleDeg
+      else:
+        apply_speed = CS.out.vEgoRaw * CV.MS_TO_KPH
+          
+      can_sends.append(BrakeSysFeatures(self.packer, frame, apply_speed))
+      can_sends.append(EngVehicleSpThrottle2(self.packer, frame, apply_speed, CS.out.gearShifter))
+      can_sends.append(ParkAid_Data(self.packer, c.active, apply_angle, CS.sappControlState, CS.out.standstill))
     
-    self.last_angle = apply_angle
-
-    #if (frame % CarControllerParams.APA_STEP) == 0:
-    can_sends.append(BrakeSysFeatures(self.packer, frame, apply_speed))
-    can_sends.append(EngVehicleSpThrottle2(self.packer, frame, apply_speed, CS.out.gearShifter))
-    can_sends.append(ParkAid_Data(self.packer, c.active, apply_angle, CS.sappControlState, CS.out.standstill))
-      
+    self.apply_angle_last = apply_angle
+    
     new_actuators = actuators.copy()
     new_actuators.steeringAngleDeg = apply_angle
 
