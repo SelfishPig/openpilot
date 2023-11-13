78ffcbcca11a999cf6fb47aff36474d8694e4012
SelfishPig <62257049+SelfishPig@users.noreply.github.com>
2023-11-13 17:22:05 -0600

    Try increasing actuator delay

--- selfdrive/car/ford/interface.py
+++ selfdrive/car/ford/interface.py
@@ -15,7 +15,7 @@ class CarInterface(CarInterfaceBase):
     ret.steerControlType = car.CarParams.SteerControlType.angle
     ret.openpilotLongitudinalControl = False
     ret.steerRateCost = 1.0
-    ret.steerActuatorDelay = 0.2
+    ret.steerActuatorDelay = 0.5
     ret.mass = 4770. * CV.LB_TO_KG + STD_CARGO_KG
     ret.steerRatio = 17
     ret.wheelbase = 3.68
