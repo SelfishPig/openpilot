d6a59af5eba1b536e5824aa45426c3c7a0251c9e
SelfishPig <62257049+SelfishPig@users.noreply.github.com>
2023-11-18 01:37:27 -0600

    Update values.py

--- selfdrive/car/ford/values.py
+++ selfdrive/car/ford/values.py
@@ -9,9 +9,9 @@ class CarControllerParams:
   ANGLE_RATE_LIMIT_UP = AngleRateLimit(speed_bp=[0.0, 15.0, 30.0], angle_v=[7.5, 1.0, 0.1])
   ANGLE_RATE_LIMIT_DOWN = AngleRateLimit(speed_bp=[0.0, 15.0, 30.0], angle_v=[7.5, 2.0, 0.2])
 
-class PingPongLimits:
-  ANGLE_RATE_LIMIT_UP = AngleRateLimit(speed_bp=[0.0, 15.0, 30.0], angle_v=[7.5, 0.1, 0.025])
-  ANGLE_RATE_LIMIT_DOWN = AngleRateLimit(speed_bp=[0.0, 15.0, 30.0], angle_v=[7.5, 0.1, 0.025])
+  # Overshoot fix
+  SMOOTH_DELTA = 5
+  SMOOTH_FACTOR = 0.1
 
 class CAR:
   F150 = "F150"
