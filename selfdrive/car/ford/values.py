87b93b06b88ee51e70fa8087104da619e710ecc2
SelfishPig <62257049+SelfishPig@users.noreply.github.com>
2023-11-18 10:30:21 -0600

    Tuning

--- selfdrive/car/ford/values.py
+++ selfdrive/car/ford/values.py
@@ -10,7 +10,7 @@ class CarControllerParams:
   ANGLE_RATE_LIMIT_DOWN = AngleRateLimit(speed_bp=[0.0, 15.0, 30.0], angle_v=[7.5, 2.0, 0.2])
 
   # Overshoot fix
-  SMOOTH_DELTA = 7
+  SMOOTH_DELTA = 5
   SMOOTH_FACTOR = 0.1
 
 class CAR:
