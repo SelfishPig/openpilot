98ed13361663d96e0bf72293764e1b1697063b98
SelfishPig <62257049+SelfishPig@users.noreply.github.com>
2023-11-22 06:49:49 -0600

    Update __init__.py

--- selfdrive/car/__init__.py
+++ selfdrive/car/__init__.py
@@ -91,11 +91,11 @@ def apply_std_steer_angle_limits(apply_angle, apply_angle_last, v_ego, LIMITS):
   rate_limits = LIMITS.ANGLE_RATE_LIMIT_UP if steer_up else LIMITS.ANGLE_RATE_LIMIT_DOWN
   angle_delta = abs(apply_angle_last - apply_angle)
   if angle_delta <= LIMITS.SMOOTH_DELTA:
-    smooth_factor = interp(angle_delta, [LIMITS.SMOOTH_DELTA, LIMITS.SMOOTH_DELTA / 2, 0], [1, LIMITS.SMOOTH_FACTOR * 2, LIMITS.SMOOTH_FACTOR])
+    smooth_factor = interp(angle_delta, [LIMITS.SMOOTH_DELTA, 0], [1, LIMITS.SMOOTH_FACTOR])
   else:
     smooth_factor = 1
-  angle_rate_lim = interp(v_ego, rate_limits.speed_bp, rate_limits.angle_v) * smooth_factor
-  return clip(apply_angle, apply_angle_last - angle_rate_lim, apply_angle_last + angle_rate_lim)
+  angle_rate_lim = interp(v_ego, rate_limits.speed_bp, rate_limits.angle_v)
+  return clip(apply_angle * smooth_factor, apply_angle_last - angle_rate_lim, apply_angle_last + angle_rate_lim)
 
 def crc8_pedal(data):
   crc = 0xFF    # standard init value
